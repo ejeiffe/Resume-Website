5c00eb4d46b49598b1ab8a6152b281050aaf6f82
Eleanor Eiffe <ejeiffe@gmail.com>
2021-05-09 13:47:42 +1000

    Sticky navbar; dropdown menu on mobile

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 /*To-Do
 Modal windows for projects section.
 */
+
+/*Sticky navbar*/
+var navbar = document.querySelector("nav");
+
+var stickyPos = navbar.offsetTop;
+
+const toggleSticky = function() {
+    if (window.pageYOffset >= stickyPos) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
+
+window.onscroll = toggleSticky;
+
+/* Open/close navbar menu on small screen*/
+var navicon = document.querySelector(".navicon");
+
+var navLinks = document.querySelector("nav ul");
+
+navicon.addEventListener("click", function() {
+    navLinks.classList.toggle("visible");
+})
