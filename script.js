fa2324b61b4c6bbe26c5f5e00b7d916b4cbdbc4a
Eleanor Eiffe <ejeiffe@gmail.com>
2021-05-12 10:55:05 +1000

    Cards and modal boxes for projects

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-/*To-Do
-Modal windows for projects section.
-*/
-
 /*Sticky navbar*/
-var navbar = document.querySelector("nav");
+const navbar = document.querySelector("nav");
 
-var stickyPos = navbar.offsetTop;
+const stickyPos = navbar.offsetTop;
 
-const toggleSticky = function() {
+const toggleSticky = ()=> {
     if (window.pageYOffset >= stickyPos) {
         navbar.classList.add("sticky");
     } else {
@@ -17,11 +13,55 @@ const toggleSticky = function() {
 
 window.onscroll = toggleSticky;
 
-/* Open/close navbar menu on small screen*/
-var navicon = document.querySelector(".navicon");
+/*Open/close navbar menu on small screens*/
+const navicon = document.querySelector(".navicon");
 
-var navLinks = document.querySelector("nav ul");
+const navLinks = document.querySelector("nav ul");
 
 navicon.addEventListener("click", function() {
     navLinks.classList.toggle("visible");
 })
+
+/*Modal windows for Projects*/
+const openModal = (modalId) => {
+    let modal = document.querySelector(modalId);
+    modal.style.display = "block";
+} 
+
+const closeModal = (modalId) => {
+    let modal = document.querySelector(modalId);
+    modal.style.display = "none";
+}
+
+/*Project 1 - Reference Manager*/
+const refManBtn = document.querySelector("#ref-man-btn");
+refManBtn.addEventListener("click", () => {
+    openModal("#ref-man-modal");
+});
+
+const refManClose = document.querySelector("#ref-man-x");
+refManClose.addEventListener("click", () => {
+    closeModal("#ref-man-modal");
+});
+
+/*Project 2 - To-Do List*/
+const toDoBtn = document.querySelector("#to-do-btn");
+toDoBtn.addEventListener("click", () => {
+    openModal("#to-do-modal");
+});
+
+const toDoClose = document.querySelector("#to-do-x");
+toDoClose.addEventListener("click", () => {
+    closeModal("#to-do-modal");
+});
+
+/*Project 3 - Analogue Generator*/
+const anaGenBtn = document.querySelector("#ana-gen-btn");
+anaGenBtn.addEventListener("click", () => {
+    openModal("#ana-gen-modal");
+});
+
+const anaGenClose = document.querySelector("#ana-gen-x");
+anaGenClose.addEventListener("click", () => {
+    closeModal("#ana-gen-modal");
+});
